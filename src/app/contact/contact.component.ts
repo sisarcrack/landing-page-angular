8a4732600c26e2d8dc79e4459a8742e9ed856e3b
César Torres <cesarsilent@gmail.com>
2024-05-28 18:12:38 +0200

    validators and patchvalue

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,16 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css'],
 })
-export class ContactComponent {
-  public usuario: any = {
-    nombre: '',
-    email: '',
+export class ContactComponent implements OnInit {
+  formularioContacto: FormGroup;
+  usuarioActivo: any = {
+    nombre: 'Pedro',
+    apellido: 'Perez',
+    dni: '12345678',
   };
+
+  constructor(private form: FormBuilder) {
+    this.formularioContacto = this.form.group({
+      nombre: ['', [Validators.required, Validators.minLength(3)]],
+      apellido: ['', [Validators.required, Validators.minLength(3)]],
+      dni: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+  ngOnInit(): void {
+    this.formularioContacto.patchValue({
+      nombre: this.usuarioActivo.nombre,
+      apellido: this.usuarioActivo.apellido,
+      dni: this.usuarioActivo.dni,
+    });
+
+    this.formularioContacto.get('nombre')?.disable();
+    this.formularioContacto.get('apellido')?.disable();
+    this.formularioContacto.get('dni')?.disable();
+  }
+
   enviar() {
-    console.log(this.usuario);
+    console.log(this.formularioContacto);
+  }
+
+  hasErrors(controlName: string, errorType: string) {
+    return (
+      this.formularioContacto.get(controlName)?.hasError(errorType) &&
+      this.formularioContacto.get(controlName)?.touched
+    );
   }
 }
